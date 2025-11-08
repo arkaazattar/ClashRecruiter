a446dba51a9161fcf861ff119f34fec1db1c8b45
Jonathan Jia <jonathanjia@hotmail.com>
2025-11-08 18:55:46 -0500

    changed trophies to leaguetiers

--- clashrecruit.py
+++ clashrecruit.py
@@ -3,7 +3,7 @@ import requests
 headers = {
 
     "Content-Type" : "application/json",
-    "Authorization" :"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzYTExOGFjLTYyMmQtNGJiNS05ZDI5LTQ4NjUwNGI1ZDgwZSIsImlhdCI6MTc1ODc3NzA3NSwic3ViIjoiZGV2ZWxvcGVyLzIyNDQwY2MxLWMyMDUtMDI0YS04NTFiLTY3ZjAxYjY4Y2E1OSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE0Mi4xODkuOTkuMjM4IiwiMTQyLjExNS4zNi4xODAiXSwidHlwZSI6ImNsaWVudCJ9XX0.C0q7BiZpjORVy61KWHb_BQzNHSJc7M81QhxURxvLijCtSwNWboFzaWD-ekgmI2-F12Ng4mw_zYGJ12GXMl1d8A" 
+    "Authorization" :"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQ2YjdiNzc2LTY4OGMtNDE1OS1iMjFmLTQzODFjNWYyYTIwNSIsImlhdCI6MTc2MjYzNjU2OSwic3ViIjoiZGV2ZWxvcGVyLzI4NjU3YTI4LTk5NWQtYmY3YS05NzRlLWEwNDViMWU2YzEzNyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE0Mi4xOTguMTAzLjE0NiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.8gLAKzH8pQ0-hDUA4aaRelqlOL48NXy5k29by0c-1PKkjgNCyfr_c8V-1Z_Huaa4jV2y2EgUhouNbZY-hmW1Bw" 
 }
 
 
@@ -14,13 +14,14 @@ def get_user():
     # print(user_json)
 
     for key in storage:
-        if key == "trophies":
-            trophies = int(storage[key])
+        if key == "leagueTier":
+            league = storage[key]["name"]
 
-            if trophies > 3000:
-                print("This user has over 3000 trophies")
+            if "P.E.K.K.A" in league:
+                print("Pekka league")
             else:
-                print("less than 3000")
+                print("not pekka")
+
 
 get_user()
 #jon was here
