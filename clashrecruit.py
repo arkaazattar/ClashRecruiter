a0d0be12f567c5dbd82983c38f266bfcf151ed1d
arkaazattar <arkaazattar@gmail.com>
2025-11-08 21:48:32 -0500

    Made 2 classes, recruiter and recruitee. Recruiter: set requirements, get requirements, print info, get clan info, post ad (NOT FINISHED), Recruitee: apply, get api_string, get stats(for checking requirements) tested works for now
    need to do: error handling but outside of function so only need to do it once per object

--- clashrecruit.py
+++ clashrecruit.py
@@ -6,31 +6,150 @@ headers = {
     "Authorization" :"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImRlYTUzNWVhLTljYTAtNDFmMC04MDJlLWIzZjk4YzAyMmMxNCIsImlhdCI6MTc2MjY0NzgxMSwic3ViIjoiZGV2ZWxvcGVyLzI4NjU3YTI4LTk5NWQtYmY3YS05NzRlLWEwNDViMWU2YzEzNyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE0Mi4xOTguMTAzLjE0NiIsIjE0Mi4xMjYuMTgwLjE2NiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.3k8c1cIEQgKpyrdKhTBtuTidotVPGTKGg4G-fRbNEvef4TxqvFAQmvIpHBjm2gvko8Sb3iRBFbZCVVw2Y_cpeA" 
 }
 class Recruiter:
-    def __init__(self, name, clan_name):
-        self.name = name
-        self.clan_name = name
+    requirements = []
 
+    def __init__(self, name, clan_tag):
+        self.name = name
+        self.clan_tag = clan_tag
 
     def print_info(self):
-        print(self.name + self.clan_name)
+        print(self.name + " " + self.clan_tag)
 
+    def get_clan_info(self):
+
+        #GET API INFO
+        response = requests.get(
+            'https://api.clashofclans.com/v1/clans/%23'+self.clan_tag, headers = headers)
+        #Store api info
+        storage = response.json()
+        print(storage)
+
+    def post_ad(self):
+        print("Hello World")
+
+    def set_requirements(self, th_level, League):
+
+        self.requirements = [th_level, League]
+
+    def get_requirements(self):
+        return self.requirements
+
+
+class Recuitee:
+
+        def __init__(self, name, user_tag):
+            self.name = name
+            self.user_tag = user_tag
+        
+        def get_stats(self):
+            acpt_input = True
+            while acpt_input:
+                response = requests.get(
+                    'https://api.clashofclans.com/v1/players/%23'+self.user_tag, headers = headers)
+                storage = response.json()
+            # print(storage)
+                try: 
+                    storage['reason'] == 'notFound'
+                    print("User doesn't exist!")
+                except:
+                    for key in storage:
+                        if key == "leagueTier":
+                            league = storage[key]["name"]
+                            #Get the int of the league that recruitee is in
+                            y=""
+                            for ch in league:
+                                if ch.isdigit():
+                                    y += ch 
+                        elif key == "townHallLevel":
+                            th_level = storage[key]
+                        
+                        if int(y) >= 0 & int(th_level) >= 0:
+                            acpt_input = False
+                        
+            # print(th_level, league)
+            stats = [th_level, int(y)]    
+            return stats
+        
+        def apply(self, Recruiter):
+            stats = self.get_stats()
+            eligibility = True
+            for i in range(2):
+
+                if stats[i-1] >= Recruiter.get_requirements()[i-1]:
+                    continue
+                else:
+                    eligibility = False
+
+            if eligibility == True:
+                print("Applied")    
+            else:
+                print("not eligible")
+
+                
 def get_user():
     # list of all league names, lowest to highest - prob not needed
     league_list = ["Skeleton", "Barbarian", "Archer", "Wizard", "Valkyrie", "Witch", "Golem", "P.E.K.K.A", "Titan", "Dragon", "Electro", "Legend"]
-    user_tag = input("Please enter your user tag: #")
+    recruiting = input("Are you recuiting? Yes or no: ")
 
-    response = requests.get(
-        'https://api.clashofclans.com/v1/players/%23'+user_tag, headers = headers)
-    storage = response.json()
-    #print(storage)
+#TESTING OBJECTS HERE !! !! !!
+    if recruiting == "Test":
+        #INITIALIZE RECRUITER TEST
+        name = input("Recruiter Name: ")
+        clan_tag = input("Clan tag: ")
+        rr = Recruiter(name, clan_tag)
+        #SET REQUIREMENTS FOR CLAN FOR RECRUITER
+        th_req = input("Enter Townhall requirement: ")
+        league_req = input("Enter League requirement number: ")
+        rr.set_requirements(int(th_req), int(league_req))
+        #INITIALIZE RECRUITEE
+        name = input("Recruitee, Please enter your name: ")
+        user_tag = input("Recruitee, Please enter your user tag: #")
+        re = Recuitee(name, user_tag)
 
-    for key in storage:
-        if key == "leagueTier":
-            league = storage[key]["name"]
+        re.apply(rr)
+
+    elif recruiting == "Yes":
+        name = input("Name: ")
+        
+        get_inputs = True
+        while get_inputs:
             
-            print(league)
-    
+            clan_tag = input("Clan tag: ")
 
+
+        user_test = Recruiter(name, clan_tag)
+        user_test.print_info()
+        # user_test.get_clan_info()
+
+
+    elif recruiting == "No":
+        name = input("Please enter your name: ")
+        user_tag = input("Please enter your user tag: #")
+
+        rt = Recuitee(name, user_tag)
+
+        rt.get_stats()
+        
+        acpt_input = True
+        while acpt_input:
+            user_tag = input("Please enter your user tag: #")
+            response = requests.get(
+                'https://api.clashofclans.com/v1/players/%23'+user_tag, headers = headers)
+            storage = response.json()
+        
+            print(storage)
+        
+            try: 
+                storage['reason'] == 'notFound'
+                print("User doesn't exist!")
+            except:
+                for key in storage:
+                    if key == "leagueTier":
+                        league = storage[key]["name"]
+                        print(league)
+                        acpt_input = False
+            
+        
 
 
 get_user()
