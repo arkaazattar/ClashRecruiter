8739dd95ef01fb045ddb4651218ad688a76d24ef
arkaazattar <arkaazattar@gmail.com>
2025-11-23 03:08:50 -0500

    fixed check_api(), added check_player()

--- clashrecruit.py
+++ clashrecruit.py
@@ -10,11 +10,21 @@ class API:
     def __init__(self, user_tag, api):
         self.token = False
         self.user_tag = user_tag
-        self.reason = 0
+        self.reason = 0 # should be string but wtv
         self.json_data = {
             "token": str(api)
         }
         #api is one time use, no point of storing it 
+    def check_player(self):
+        url = f"https://api.clashofclans.com/v1/players/%23{self.user_tag}"
+        response = requests.get(url, headers=headers)
+        self.storage = response.json() 
+
+        if self.storage.get("reason") == "notFound":
+            self.reason = "Player tag is incorrect" 
+            return False
+        return True
+    
     def check_player_api(self):
         #logic might be broken
         url = f"https://api.clashofclans.com/v1/players/%23{self.user_tag}/verifytoken"
@@ -22,10 +32,7 @@ class API:
         response = requests.post(url, headers=headers, json = self.json_data)
         self.storage = response.json() # Important!! this storage now holds whether the api key is valid or not
 
-        if self.storage.get("reason") == "notFound":
-            self.reason = "Player tag is incorrect" 
-
-        elif self.storage.get("status") == "invalid":
+        if self.storage.get("status") == "invalid":
             self.reason = "API Token is incorrect"
 
         elif self.storage.get("status") == "ok":
@@ -62,6 +69,8 @@ class Recruiter: # error checking needs to be done out of class
         #Store api info
         self.storage = response.json()
         #print(self.storage)
+        if self.storage.get("reason") == "notFound":
+            return
         large_list = self.storage.get("items")
         #print(large_list)
         for player_info in large_list:    
@@ -69,7 +78,6 @@ class Recruiter: # error checking needs to be done out of class
                 if player_info.get("role").lower() in roles:
                   is_leader = True
         return is_leader     
-
 
     def post_ad(self):
         print(self.requirements)
@@ -101,6 +109,10 @@ def check_api():
     valid_api = False
     while valid_api == False:
         user_tag = input("Please enter your player tag: #")
+        tag_check = API(user_tag, "")
+        if tag_check.check_player() == False:
+            print(f"{tag_check.reason}, try again.")
+            continue
         api = input("Please enter your API token: ")
         user = API(user_tag, api)
         user.check_player_api()
@@ -113,12 +125,15 @@ def recruiting(user_tag):
     invalid_clan = True
     while invalid_clan:
         clan_tag = input("Enter Clan Tag: #")
-        if clan_tag[0] == '#':
-            clan_tag = clan_tag[1:]
+        try:   
+            if clan_tag[0] == '#':
+                clan_tag = clan_tag[1:]
+        except:
+            continue
         clan = Recruiter(user_tag, clan_tag)
-        
         if clan.check_if_leader() == True:
             invalid_clan = False
+        else: print("Clan not found, try again?")
     return
         #print(clan.storage)
 
@@ -127,12 +142,15 @@ def recruitee():
     while invalid_input:
         looking_for_clan = input("Are you looking for a clan? Yes or no: ").lower()
         if looking_for_clan == "yes" or  looking_for_clan == "no":
-            invalid_input = False
+            if looking_for_clan == "no":
+                exit() # kill program ? prob wont be needed when made into website
+            else: invalid_input = False
         else: print("Invalid input")
+    return
 
 def get_user():
     response = ask_if_recruiting()
-    user_tag =check_api()
+    user_tag = check_api()
     if response == "yes":
         recruiting(user_tag)
     if response == "no":
@@ -140,8 +158,7 @@ def get_user():
 
     
 
-
-#TESTING OBJECTS HERE !! !! !!
+#TESTING OBJECTS HERE !! !! !! ts dont work anymore </3
     if recruiting == "test":
         #INITIALIZE RECRUITER TEST
         name = input("Recruiter Name: ")
