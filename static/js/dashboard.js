292fa4476623ead3eae290b71f1334d6d5596a0f
Arkaaz <arkaazattar@gmail.com>
2026-01-01 21:33:41 -0500

    added buttons and styles
    Co-authored-by: Jonathan Jia <jonathanjia@hotmail.com>

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,2 +1,8 @@
-document.getElementById("result").innerText =
-      "able to recruit: " + result.recruit_status ;
+// document.getElementById("result").innerText =
+//       "able to recruit: " + result.recruit_status ;
+
+const button = document.getElementById("recruit");
+
+button.addEventListener("click", () => {
+    window.location.href ="/recruiter";
+});
--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,3 @@
-
     const form = document.getElementById("dataForm");
     
     form.addEventListener("submit", async function(event){
